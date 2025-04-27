543de325e7cdd2bff6626bd8cbb25c47e1c19754
AnamShaikh05 <shkanam2005@gmail.com>
2025-04-27 13:38:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -476,5 +476,8 @@ def delete_dashboard(dashboard_id):
     finally:
         conn.close()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
